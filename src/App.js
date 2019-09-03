7677f3d1eedead8d9d00a1cdd1161e240ddbd46a
cutyapple <dbtldhs0915@naver.com>
2019-09-03 20:14:24 +0900

    #2.3 .map Recap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React from "react";
 
-function Food({fav}){
+const ObjecList = [{
+      id:1,
+      name:"drinks",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ8gjYqzMx3uR5WkTTAQYOSZXVeZH67PQD9qkYWB5V5EZTtVzL"
+    },
+    {
+      id:2,
+      name:"flowers",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqEKPcTGo5Fv00HSupeZ_Qh3M0bHu3EhQrJnSEZd45gnk_Rm9W" 
+    },
+    {
+      id:3,
+      name:"vip",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKwBNxq06ibKUPcD8d-EwbiGJGfYqJKTf2zD8nrJT_tJu2OKmTUw" 
+    }
+  ];
+
+function Objec({name, picture}){
   return(
-    <div>
-      <h1>{fav}</h1>
-    </div>
-  )
-}
+      <div>
+        <h2>It is {name}</h2>
+        <img  src={picture} alt={name}/>
+      </div>
+  );
+}  
 
 function App() {
   return (
     <div>
-      <h1>Hello!</h1>
-      <Food fav="kimchi"/>
-      <Food fav="asfd"/>
-      <Food fav="fasd"/>
-      <Food fav="asfas"/>
+      {ObjecList.map(what => (
+        <Objec key={what.id} name={what.name} picture={what.image} />
+      ))}
     </div>  
   );
 }
