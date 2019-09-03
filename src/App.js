24a14decca69a972a04449e5923a8225d39a6b35
cutyapple <dbtldhs0915@naver.com>
2019-09-03 22:50:14 +0900

    #3_0

--- src/App.js
+++ src/App.js
@@ -1,50 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ObjecList = [{
-      id:1,
-      name:"drinks",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ8gjYqzMx3uR5WkTTAQYOSZXVeZH67PQD9qkYWB5V5EZTtVzL",
-      rating:5
-    },
-    {
-      id:2,
-      name:"flowers",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqEKPcTGo5Fv00HSupeZ_Qh3M0bHu3EhQrJnSEZd45gnk_Rm9W" ,
-      rating:4.4
-    },
-    {
-      id:3,
-      name:"vip",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKwBNxq06ibKUPcD8d-EwbiGJGfYqJKTf2zD8nrJT_tJu2OKmTUw" ,
-      rating:4.7
-    }
-  ];
+class App extends React.Component{
+  state = {
+    count: 0
+  };
 
-function Objec({name, picture, score}){
-  return(
+  add = () =>{
+    console.log("add");
+  };
+  minus = () =>{
+    console.log("minus");
+  };
+
+  render(){
+    return(
       <div>
-        <h2>It is {name}</h2>
-        <img  src={picture} alt={name}/>
-        <h3>It's score is {score}</h3>
+        <h1>The Number is {this.state.count}</h1>
+        <button onClick={this.add()}>Add</button>
+        <button onClick={this.minus()}>Minus</button>
       </div>
-  );
-}  
-
-Objec.propTypes = {
-  name:PropTypes.string.isRequired,
-  picture:PropTypes.string.isRequired,
-  score:PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div>
-      {ObjecList.map(what => (
-        <Objec key={what.id} name={what.name} picture={what.image} score={what.rating}/>
-      ))}
-    </div>  
-  );
+    )
+  }
 }
+
+
 
 export default App;
