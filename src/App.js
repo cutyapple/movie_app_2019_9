d0d3d63589c30200eda631c25ab88427f689f5e2
cutyapple <dbtldhs0915@naver.com>
2019-09-04 09:03:41 +0900

    #3.3_Planning_the_Movie_Component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,27 @@
 import React from "react";
 
-class App extends React.Component{
+  class App extends React.Component{
   state = {
-    count: 0
+    isLoading : true
   };
-
-  constructor(props){
-    super(props);
-    console.log("Hello?");
-  }
 
   componentDidMount(){
-    console.log("component rendering");
+    setTimeout(() => {
+      this.setState({isLoading:false});
+    }, 6000);
   }
-
-  componentDidUpdate(){
-    console.log("component updating");
-  }
-
-
-  add = () =>{
-    this.setState(current => ({
-      count: current.count + 1
-    }));
-  };
-  minus = () =>{
-    this.setState(current => ({
-      count: current.count - 1
-    }));
-  };
 
   render(){
-    console.log("I'm rendering");
+    const {isLoading} = this.state;
+
     return(
       <div>
-        <h1>The Number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        {isLoading ? "Loading..." : "We are ready"}
       </div>
-    )
+    );
   }
 }
 
 export default App;
 
-/*#3_2 Component Life Cycle*/
+/*#3.3_Planning_the_Movie_Component*/
